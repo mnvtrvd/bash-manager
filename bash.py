24562828cb580f3acdcf4e84af7ad61f8b8e4b59
Manav Trivedi <manavatrivedi@gmail.com>
2021-01-19 04:41:16 -0500

    implemented global aliases to use bash manager from anywhere

--- bash.py
+++ bash.py
@@ -8,6 +8,7 @@ from shutil import copyfile
 BASH_PROFILE = ".zsh_aliases"
 HOME = str(Path.home())
 PATH = HOME + "/" + BASH_PROFILE
+BACKUP_PATH = HOME + "/bash-manager/" + BASH_PROFILE
 
 # opens bash file in vim or vscode (defaults to vim)
 def open_bash(code=False):
@@ -26,14 +27,14 @@ def refresh():
 # saves a backup of your current bash profile to this directory (just in case)
 def backup():
     if os.path.exists(PATH):
-        copyfile(PATH, BASH_PROFILE)
+        copyfile(PATH, BACKUP_PATH)
         print("successfully backed up bash profile")
     else:
         print("ERROR: a bash profile with provided name '" + BASH_PROFILE + "' does not exist")
 
 # reverts bash profile to saved version from earlier
 def revert():
-    copyfile(BASH_PROFILE, PATH)
+    copyfile(BACKUP_PATH, PATH)
     print("successfully reverted back to earlier bash profile")
 
 # checks if a provided alias already exists and returns corresponding action
@@ -129,21 +130,25 @@ def add_alias(alias, action):
     f.close()
     print("added new " + new_alias)
 
-if not os.path.exists(BASH_PROFILE):
+if not os.path.exists(BACKUP_PATH):
     print("no backup detected, taking one now just in case")
     backup()
 
 parser = argparse.ArgumentParser(description='Modifies your existing bash profile and allows for quick alias management.')
+parser.add_argument('--open', nargs='?', type=int, const=1, default=0, help='this opens your bash profile in vim.')
 parser.add_argument('--backup', nargs='?', type=bool, const=True, default=False, help='this creates a backup of your current bash profile just in case something goes wrong later.')
 parser.add_argument('--revert', nargs='?', type=bool, const=True, default=False, help='this reverts your bash profile back to a previously working state.')
+parser.add_argument('--refresh', nargs='?', type=bool, const=True, default=False, help='this refreshes your bash profile.')
 parser.add_argument('--list', nargs='?', type=bool, const=True, default=False, help='this will list all of the aliases in your bash profile.')
 parser.add_argument('--add', nargs=2, type=str, default=["",""], help='this will add given alias and action to your bash profile.')
 parser.add_argument('--rm', type=str, default="", help='this will remove the given alias from your bash profile.')
 parser.add_argument('--whatami', type=str, default="", help='this will define a provided alias.')
 args = parser.parse_args()
 
+OPEN = args.open
 BACKUP = args.backup
 REVERT = args.revert
+REFRESH = args.refresh
 LIST = args.list
 ADD_ALIAS = args.add[0]
 ADD_ACTION = args.add[1]
@@ -152,24 +157,40 @@ WHATAMI = args.whatami
 
 count = 0
 
+if OPEN > 0: count += 1
 if BACKUP: count += 1
 if REVERT: count += 1
+if REFRESH: count += 1
 if LIST: count += 1
 if ADD_ALIAS != "" or ADD_ACTION != "": count += 1
 if RM != "": count += 1
 if WHATAMI != "": count += 1
 
-if count != 1: print("ERROR: please execute exactly one command at a time")
-elif BACKUP: backup()
-elif REVERT: revert()
-elif LIST: list_alias()
-elif ADD_ALIAS != "" or ADD_ACTION != "": add_alias(ADD_ALIAS, ADD_ACTION)
-elif RM != "": rm_alias(RM)
+if count == 0:
+    print("you need to provide at least 1 command to work (use -h or --help for assistance)")
+elif count > 1:
+    print("ERROR: please execute exactly one command at a time")
+elif OPEN > 0:
+    if OPEN == 1:
+        open_bash()
+    else:
+        open_bash(True)
+elif BACKUP:
+    backup()
+elif REVERT:
+    revert()
+    refresh()
+elif REFRESH:
+    refresh()
+elif LIST:
+    list_alias()
+elif ADD_ALIAS != "" or ADD_ACTION != "":
+    add_alias(ADD_ALIAS, ADD_ACTION)
+elif RM != "":
+    rm_alias(RM)
 elif WHATAMI != "":
     action = alias_exists(WHATAMI)
     if action == "":
         print("ERROR: provided alias does not exist")
     else:
         print(action)
-
-refresh()
