d4bd3afe7732859d2a79b1628eceba52b317fd7e
Joe Michelini <josephmichelini@gmail.com>
2021-01-20 16:13:26 -0500

    handle shutil exception

--- setup.py
+++ setup.py
@@ -8,9 +8,16 @@ def setup():
     settings = {"BASH_PROFILE": b_profile}
     with open("config.json", "w+") as config:
         json.dump(settings, config)
-    shutil.move(os.getcwd(), str(Path.home()))
-    print("\nbash-manager has been moved to ~/bash-manager\n")
-    print("""to enable default behavior, please execute: 
-                $ python3 ~/bash-manager/bash.py --default\n""")
+    
+    HOME = str(Path.home())
+    CUR_DIR = os.getcwd()
+    if CUR_DIR != HOME + '/bash-manager':
+        shutil.move(CUR_DIR, HOME)
+        print("\nbash-manager has been moved to ~/bash-manager\n")
+        print("""to enable default behavior, please execute: 
+                    $ python3 ~/bash-manager/bash.py --default\n""")
+    else:
+        print(f"\nbash profile changed to {b_profile}\n")
+
 if __name__ == "__main__":
     setup()
